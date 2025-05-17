dd0ddc65d5355afa01f4d32c3280e40e1963e0bc
JosmenDev <josmendev@gmail.com>
2025-05-16 19:21:52 -0500

    fix(server): correction module app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,26 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
-import { AdminSubscriptionModule } from './admin-subscription/admin-subscription.module';
-import { AdminPersonModule } from './admin-person/admin-person.module';
 import { AdminSubscriptionsModule } from './admin-subscriptions/admin-subscriptions.module';
 import { AdminPersonsModule } from './admin-persons/admin-persons.module';
 import { AdminServicesModule } from './admin-services/admin-services.module';
-import { RegisterReservationsModule } from './register-reservations/register-reservations.module';
 import { ReservationsModule } from './reservations/reservations.module';
 import { AuthModule } from './auth/auth.module';
 import { EmailsModule } from './emails/emails.module';
 import { ExternalApisModule } from './external-apis/external-apis.module';
-import { PersonsModule } from './persons/persons.module';
 import { AdminProgrammingModule } from './admin-programming/admin-programming.module';
 
 @Module({
-  imports: [CommonModule, AdminSubscriptionModule, AdminPersonModule, AdminSubscriptionsModule, AdminPersonsModule, AdminServicesModule, RegisterReservationsModule, ReservationsModule, AuthModule, EmailsModule, ExternalApisModule, PersonsModule, AdminProgrammingModule],
+  imports: [
+    CommonModule,
+    AdminSubscriptionsModule,
+    AdminPersonsModule,
+    AdminServicesModule,
+    ReservationsModule,
+    AuthModule,
+    EmailsModule,
+    ExternalApisModule,
+    AdminProgrammingModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/common/helpers/paginate.helper.ts
+++ src/common/helpers/paginate.helper.ts
@@ -3,7 +3,7 @@ import { PaginationDto } from '../dto/pagination.dto';
 import { Paginated } from '../interfaces/paginated.interface';
 
 export const paginate = async <T>(
-  queryBuilder: SelectQueryBuilder<T>,
+  queryBuilder: SelectQueryBuilder<T extends object ? T : any>,
   paginationDto: PaginationDto,
 ): Promise<Paginated<T>> => {
   const { page = 1, limit = 5 } = paginationDto;
